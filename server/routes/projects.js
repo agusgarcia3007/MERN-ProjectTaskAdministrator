d719a9ffb2b4244213559fb60d70b25281c8eb08
Agustin García dos Santos <agusgarcia3007@gmail.com>
2021-11-04 13:45:10 -0300

    everything working. able to update projects with authentication

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
+const auth = require('../middleware/authMiddle');
+const { check } = require('express-validator')
 
 // /api/projects
+
 //create a project
 router.post('/',
+    auth,
+    [
+        check('name', 'project name required').not().isEmpty()
+    ],
     projectController.createProject
 );
+//get owner's projects
+router.get('/',
+    auth,
+    projectController.getProjects
+);
+
+//update project by ID
+router.put('/:id',
+    auth,
+    [
+        check('name', 'project name required').not().isEmpty()
+    ],
+    projectController.updateProject
+)
 
 
 module.exports = router;
