500fca47c1336f19bb29d13f8a961e9ed07e997d
Agustin García dos Santos <agusgarcia3007@gmail.com>
2021-11-08 11:44:56 -0300

    backend finished

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(express.json({extended : true }))
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
+app.use('/api/tasks', require('./routes/tasks'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
